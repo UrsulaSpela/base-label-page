c80f8a936928d6ac8c107f6c2e722227089ee678
Ursula <77993714+UrsulaSpela@users.noreply.github.com>
2021-05-20 13:58:07 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function validation(){
+  var from = document.getElementById('form');
+  var email = document.getElementById('email').value;
+  var error = document.getElementById('error');
+  var pattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+
+  if(email.match(pattern)){
+    error.style.dispay = "none";
+  } else{
+    error.style.dispay = "flex";
+  }}
